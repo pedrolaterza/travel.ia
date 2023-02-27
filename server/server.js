204b13365a4a4bc003f7f47a5ad4274a82d1f8d3
Pedro Laterza <laterza.henrique@gmail.com>
2023-02-27 12:44:36 -0300

    trying to fix the error

--- server/server.js
+++ server/server.js
@@ -116,8 +116,10 @@ app.post('/', async (req, res) =>{
             model: "text-davinci-003",
             prompt: `${dontDo} ${botProfile} ${botFunction} ${prompt}`,
             temperature: 0,
-            max_tokens: 3600,
+            max_tokens: 3000,
             top_p:1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
             stop:["\"\"\""],
 
         });

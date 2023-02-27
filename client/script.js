1813b2f7ada6d4aec19c29a4434e50ab1649467d
Pedro Laterza <laterza.henrique@gmail.com>
2023-02-27 13:16:37 -0300

    I wont giveup

--- client/script.js
+++ client/script.js
@@ -107,12 +107,13 @@ const handleSubmit = async (e) => {
   }
 }
 
-
 form.addEventListener('submit', handleSubmit);
-form.addEventListener('keyup').addEventListener('click', (e) => {
-    e.preventDefault();
+
+form.addEventListener('keyup', (e) => {
+  if(e.keyCode === 13){
     handleSubmit(e);
-  });
+  }
+})
 
 /*  BACKGROUND SWITH */
 
